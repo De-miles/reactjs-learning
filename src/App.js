e3a2eb4fdd23b9e008ae58452fd3cb83da9d1413
De-miles <miles998321@gmail.com>
2024-07-11 01:07:26 +0700

    demo call api - interceptors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import './App.scss';
 import TodoFeature from 'features/Todo/pages';
 import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
 import AlbumFeature from 'features/Album/pages';
 import NotFound from 'components/NotFound';
+import productApi from 'api/productApi';
 
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="app">
       Header
