c34e7e9b226b6af72e0e20b7563cade3eb6c010e
De-miles <miles998321@gmail.com>
2024-07-11 15:04:02 +0700

    form validation w uyup - show error message

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -13,8 +13,21 @@ InputField.propTypes = {
 
 function InputField(props) {
   const { form, name, label, disabled } = props;
+  const { errors, formState } = form;
+  const hasError = formState.touched[name] && errors[name];
 
-  return <Controller name={name} control={form.control} as={TextField} fullWidth label={label} disabled={disabled} />;
+  return (
+    <Controller
+      name={name}
+      control={form.control}
+      as={TextField}
+      fullWidth
+      label={label}
+      disabled={disabled}
+      error={!!hasError}
+      helperText={errors[name]?.message}
+    />
+  );
 }
 
 export default InputField;
