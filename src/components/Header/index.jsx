914f9052a2af8e5b20d718e22b2acd564914f640
De-miles <miles998321@gmail.com>
2024-07-15 16:39:31 +0700

    close icon for register form

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,6 +14,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Register from 'features/Auth/components/Register';
+import { Close } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,6 +29,13 @@ const useStyles = makeStyles((theme) => ({
   link: {
     color: '#fff',
     textDecoration: 'none',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: theme.spacing(1),
+    right: theme.spacing(1),
+    color: theme.palette.grey[500],
+    zIndex: 1,
   },
 }));
 
@@ -75,14 +83,12 @@ export default function Header() {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
+        <IconButton className={classes.closeButton} onClick={handleClose}>
+          <Close />
+        </IconButton>
         <DialogContent>
           <Register closeDialog={handleClose} />
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
