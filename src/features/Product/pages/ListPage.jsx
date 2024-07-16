172741cb418a2519ac7f4b812c07107370a15456
De-miles <miles998321@gmail.com>
2024-07-16 10:15:42 +0700

    setup Product Feature

---
+++ src/features/Product/pages/ListPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+
+ListPage.propTypes = {};
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  left: {
+    width: '250px',
+  },
+  right: {
+    flex: '1 1 auto',
+  },
+}));
+
+function ListPage(props) {
+  const classes = useStyles();
+  return (
+    <Box>
+      <Container>
+        <Grid container>
+          <Grid item className={classes.left}>
+            <Paper elevation={0}>Left column</Paper>
+          </Grid>
+          <Grid item className={classes.right}>
+            <Paper elevation={0}>Right column</Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default ListPage;
