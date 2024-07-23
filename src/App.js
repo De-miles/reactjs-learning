426a7addc52fd026e6834b7beba40ed5195718a2
De-miles <miles998321@gmail.com>
2024-07-23 17:52:55 +0700

    setup Cart Feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from 'components/NotFound';
 import CounterFeature from 'features/Counter';
 import Header from 'components/Header';
 import ProductFeature from 'features/Product';
+import CartFeature from 'features/Cart';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/todos" component={TodoFeature} />
         <Route path="/albums" component={AlbumFeature} />
         <Route path="/products" component={ProductFeature} />
+        <Route path="/cart" component={CartFeature} />
         <Route component={NotFound} />
       </Switch>
     </div>
