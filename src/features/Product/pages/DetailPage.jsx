c78cb46bc2e109c8084fca6af24ae62d7eb9d164
De-miles <miles998321@gmail.com>
2024-07-23 15:58:18 +0700

    implement component productMenu

--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -6,6 +6,7 @@ import { useRouteMatch } from 'react-router-dom';
 import useProductDetail from '../hooks/useProductDetail';
 import ProductInfo from '../components/ProductInfo';
 import AddToCartForm from '../components/AddToCartForm';
+import ProductMenu from '../components/ProductMenu';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -57,6 +58,7 @@ function DetailPage() {
             </Grid>
           </Grid>
         </Paper>
+        <ProductMenu />
       </Container>
     </Box>
   );
